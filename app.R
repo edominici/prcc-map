0ab1e069252e2ae5e4820266c4ef0fae549f605a
macchiatoyn <shinyyn@gmail.com>
2018-11-27 19:22:18 -0600

    Add zipcode search

--- app.R
+++ app.R
@@ -13,26 +13,34 @@ library(googlesheets)
 library(leaflet)
 library(RDSTK)
 library(leaflet.extras)
+library(zipcode)
+data(zipcode)
 
 
 
 # Define UI for application that draws a histogram
 ui <- fluidPage(
+  tags$head(
+    # Include our custom CSS
+    #includeCSS("style.css")
+  ),
+  
   #title
   titlePanel("PRCC Asset Map"),
   
   sidebarLayout(
-    
+
     #filters here
     sidebarPanel(
-      
+      selectInput('zip_list', 'Select Your Community','60201')
     ),
+
+    
     mainPanel(
-      
       #map
       leafletOutput('prcc_map', height = 1000)
       #, tableOutput("results")
-      
+
     )
     
   )
@@ -40,7 +48,7 @@ ui <- fluidPage(
 )
 
 # Define server logic required to draw a histogram
-server <- function(input, output) {
+server <- function(input, output,session) {
   
   #Reading Data
   spreadSheet <- gs_title("Copy of PRCC Asset Data Template")
@@ -49,7 +57,7 @@ server <- function(input, output) {
   #Preparing Data
   #Geocode Addresses
   #Create full Address for geocoding
-  mapData$fullAddress <- paste(mapData$Address, mapData$City, mapData$State, sep = ", ")
+  mapData$fullAddress <- paste(mapData$Address, mapData$City, mapData$State, mapData$Zip,sep = ", ")
   for(i in 1:nrow(mapData))
   {
     # Print("Working...")
@@ -59,19 +67,36 @@ server <- function(input, output) {
     mapData$geoAddress[i] <- as.character(result[3])
   }
   
+  # get zipcodes from google sheet
+  get_zipcodes = reactive({
+    zip_data = mapData$Zip
+  })
+  
+  observe({
+    updateSelectInput(session, "zip_list",choices = get_zipcodes()
+  )})
+
+  
+ 
   #Map
   output$prcc_map <- renderLeaflet({
+    
+    # get longitude and latitude of selected zipcode
+    print(as.numeric(input$zip_list))
+    input_Zip = subset(zipcode, zip==as.numeric(input$zip_list),c("longitude", "latitude"))
+   
     #Map
     prccMap <- mapData %>%
+      
       leaflet() %>%
       addTiles()%>%
-      setView(lat = 41.8781, lng = -87.6298, zoom = 12) %>%
-      addMarkers(lat = ~lat, lng = ~lon, popup = ~paste("<strong><a href='", Website, "' target='_blank'>", Name, "</a></strong><br>", Address, "<br>", City, ", ", State, Zip), clusterOptions = markerClusterOptions())%>%
-      addResetMapButton() %>%
-      addSearchOSM() 
-    prccMap
-    })
+      setView(lat = as.numeric(input_Zip["latitude"]), lng =as.numeric(input_Zip["longitude"]), zoom = 15) %>%
 
+      addMarkers(lat = ~lat, lng = ~lon, popup = ~paste("<strong><a href='", Website, "' target='_blank'>", Name, "</a></strong><br>", Address, "<br>", City, ", ", State, Zip), clusterOptions = markerClusterOptions())%>%
+      #addResetMapButton() %>%
+      addSearchOSM(options = searchOptions(position = "topright"))
+    prccMap
+  })
  
 }
 
