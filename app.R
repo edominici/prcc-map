87612f9aba9b9ce6c2c1cfb8a992707282ba7819
Erin Dominici <erindominici@gmail.com>
2018-12-05 01:42:49 -0600

    added filter to ui and made fixes to zip search

--- app.R
+++ app.R
@@ -13,8 +13,23 @@ library(googlesheets)
 library(leaflet)
 library(RDSTK)
 library(leaflet.extras)
+library(ggmap)
 library(zipcode)
+library(tidyr)
 data(zipcode)
+
+spreadSheet <- gs_title("DominiciE_PRCC_Chicago2017")
+mapData <- as.data.frame(gs_read(ss=spreadSheet, ws = "Locations"))
+sepData2 <- separate_rows(mapData, "Payment Types", sep = ",")
+sepData1 <- separate_rows(sepData2, "Services Offered", sep = ",")
+sepData <- separate_rows(sepData1, "Populations Served", sep = ",")
+head(sepData)
+
+service_choices <- unique(as.character(sepData$"Services Offered"))
+spanish_choices <- unique(as.character(sepData$"Spanish"))
+payment_choices <- unique(as.character(sepData$"Payment Types"))
+zip_choices <- c(60007, 60018, 60068, 60106, 60131, 60176, 60601, 60602, 60603, 60604, 60605, 60606, 60607, 60608, 60609, 60610, 60611, 60612, 60613, 60614, 60615, 60616, 60617, 60618, 60619, 60620, 60621, 60622, 60623, 60624, 60625, 60626, 60628, 60629, 60630, 60631, 60632, 60633, 60634, 60636, 60637, 60638, 60639, 60640, 60641, 60642, 60643, 60644, 60645, 60646, 60647, 60649, 60651, 60652, 60653, 60654, 60655, 60656, 60657, 60659, 60660, 60661, 60706, 60707, 60714, 60804, 60827)
+
 
 
 
@@ -29,84 +44,87 @@ ui <- fluidPage(
   titlePanel(
     tags$div(id = "pagetitle",
              tags$h1("PRCC",tags$span(id = "subtitle","Asset Map"))
-             )
+    )
     
   ),
   
   sidebarLayout(
-      #filters here
+    #filters here
     sidebarPanel(id = 'sidebar',
-      tags$div(id='zipsearch',
-        selectInput('zip_list', 'SELECT YOUR COMMUNITY','60201')
-        )
+                 tags$div(id='zipsearch',
+                          selectInput(
+                            'zipInput', 'SELECT YOUR ZIPCODE', choices = c("Select zip code", zip_choices), selected = "Select zip code")
+                 ),
+                 
+                 selectInput(
+                   'servicesInput', 'Services:', choices = c(service_choices[!is.na(service_choices)]),
+                   selectize = TRUE, multiple = TRUE
+                 ),
+                 
+                 selectInput(
+                   'languagesInput', 'Language:', choices = c(service_choices[!is.na(spanish_choices)]), 
+                   selectize = TRUE, multiple = TRUE
+                 ),
+                 
+                 selectInput(
+                   'paymentInput', 'Payment Options:', choices = c(service_choices[!is.na(payment_choices)]), 
+                   selectize = TRUE, multiple = TRUE
+                 )
     ),
-
-
+    
+    
     
     mainPanel(
       #map
-      leafletOutput('prcc_map', height = 1000)
+      leafletOutput('prccMap', height = 1000)
       #, tableOutput("results")
-
+      
     )
     
   )
- 
+  
 )
 
 # Define server logic required to draw a histogram
 server <- function(input, output,session) {
   
-  #Reading Data
-  spreadSheet <- gs_title("Copy of PRCC Asset Data Template")
-  mapData <- as.data.frame(gs_read(ss=spreadSheet, ws = "Master Sheet"))
-  
-  #Preparing Data
-  #Geocode Addresses
-  #Create full Address for geocoding
-  mapData$fullAddress <- paste(mapData$Address, mapData$City, mapData$State, mapData$Zip,sep = ", ")
-  for(i in 1:nrow(mapData))
-  {
-    # Print("Working...")
-    result <- street2coordinates(mapData$fullAddress[i])
-    mapData$lon[i] <- as.numeric(result["longitude"])
-    mapData$lat[i] <- as.numeric(result["latitude"])
-    mapData$geoAddress[i] <- as.character(result[3])
-  }
   
   # get zipcodes from google sheet
-  get_zipcodes = reactive({
-    zip_data = mapData$Zip
-  })
-  
-  observe({
-    updateSelectInput(session, "zip_list",choices = get_zipcodes()
-  )})
 
   
- 
+  
+  
   #Map
-  output$prcc_map <- renderLeaflet({
+  output$prccMap <- renderLeaflet({
     
     # get longitude and latitude of selected zipcode
-    print(as.numeric(input$zip_list))
-    input_Zip = subset(zipcode, zip==as.numeric(input$zip_list),c("longitude", "latitude"))
    
+    
     #Map
     prccMap <- mapData %>%
       
       leaflet() %>%
       addTiles()%>%
-      setView(lat = as.numeric(input_Zip["latitude"]), lng =as.numeric(input_Zip["longitude"]), zoom = 15) %>%
-
-      addMarkers(lat = ~lat, lng = ~lon, popup = ~paste("<strong><a href='", Website, "' target='_blank'>", Name, "</a></strong><br>", Address, "<br>", City, ", ", State, Zip), clusterOptions = markerClusterOptions())%>%
+     # setView(lat = 41.8781, lng = -87.6298, zoom = 12) %>%
+      addMarkers(lat = ~Lat, lng = ~Lng, popup = ~paste("<strong><a href='", Website, "' target='_blank'>", Name, "</a></strong><br>", Street, "<br>", City, ", ", State, Zip), clusterOptions = markerClusterOptions())%>%
       #addResetMapButton() %>%
       addSearchOSM(options = searchOptions(position = "topright"))
     prccMap
   })
- 
+  
+  
+  
+  observeEvent(input$zipInput, {
+    
+    print(as.numeric(input$zipInput))
+    input_Zip = subset(zipcode, zip==as.numeric(input$zipInput),c("longitude", "latitude"))
+    print(input_Zip)
+    proxy <-leafletProxy("prccMap", data = mapData)
+    proxy %>%
+      setView(lat = as.numeric(input_Zip["latitude"]), lng =as.numeric(input_Zip["longitude"]), zoom = 15)
+  })
+  
 }
 
 # Run the application 
 shinyApp(ui = ui, server = server)
-
